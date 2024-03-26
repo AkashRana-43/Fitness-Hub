1e7794ae0c1f1746bc725b68c0fb39457a0c3016
Akash Rana <cielo.rana113@gmail.com>
2024-03-26 15:57:22 +1030

    Project Initial

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import AboutUs from './pages/AboutUs';
+
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={ <Home /> } />
+          <Route path='/aboutus' element={ <AboutUs /> } />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
